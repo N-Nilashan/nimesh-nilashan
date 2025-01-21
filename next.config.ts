1ff73e267e966ee852f6953923958b6343bc0c05
N-Nilashan <informal.nimesh@gmail.com>
2025-01-22 00:07:29 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,8 @@
-import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Any other configuration for Next.js goes here
 };
 
-const sentryConfig = {
-  sentryBuildOptions: {
-    hideSourceMaps: true,
-  },
-  org: "jsm-cz",
-  project: "javascript-nextjs",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  tunnelRoute: "/monitoring",
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-};
 
-export default withSentryConfig(nextConfig, sentryConfig);
+

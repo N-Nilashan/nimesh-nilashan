734078cb120214f1aa915e2081a003fc9dba5e1f
N-Nilashan <informal.nimesh@gmail.com>
2025-01-22 01:30:04 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,13 @@ const nextConfig: NextConfig = {
   // Any other configuration for Next.js goes here
 };
 
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 

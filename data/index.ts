a1e256d758b1dc8060e1689b9ecfc1d74a34d2ed
N-Nilashan <informal.nimesh@gmail.com>
2025-01-22 22:09:42 +0530

    Initial commit

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building an expense tracker!",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -212,6 +212,6 @@ export const socialMedia = [
   {
     id: 3,
     img: "/link.svg",
-    link:"www.linkedin.com/in/nimesh-nilashan"
+    link:"https://www.linkedin.com/in/nimesh-nilashan/"
   },
 ];
